648b77dd28067fbbb941ef2a09b8fa360ab503f7
cmeloni <carlo.meloni@abinsula.com>
2023-06-09 11:52:42 +0200

    fix: minor  fixes

--- src/components/general/GameCard.tsx
+++ src/components/general/GameCard.tsx
@@ -1,4 +1,5 @@
 import { Game } from "../../interfaces/Games";
+import "./GameCard.css";
 
 interface GameCardProps {
     game: Game;
@@ -6,11 +7,28 @@ interface GameCardProps {
 
 const GameCard = ({ game }: GameCardProps) => {
     return (
-        <div>
+        <div className="game-card">
             <h2>{game.name}</h2>
             <p>{game.address}</p>
-            <p>{game.numberOfPeople}</p>
+            <p>Spettatori:  {game.numberOfPeople}</p>
             <p>{game.time ? game.time : "No time assinged yet."}</p>
+            <p>{game.date.toString().split("T")[0].split('-').reverse().join("/")}</p>
+            {/* <Grid
+                key={game._id}
+                xs={3}
+                sx={{
+                  border: "1px solid black",
+                  borderRadius: "5px",
+                  margin: "1rem",
+                  padding: "1rem",
+                  width: "27%",
+                }}
+              >
+                <Link to={`/game/${game._id}`}>
+                  <p>{game.name ? game.name : "Nessun Nome"}</p>
+                </Link>
+                <p>{game.address}</p>
+              </Grid> */}
         </div>
     )
 }
--- src/features/games/GamePage.tsx
+++ src/features/games/GamePage.tsx
@@ -1,6 +1,7 @@
 import { Container, Grid } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useAppSelector } from "../../store/store";
+import GameCard from "../../components/general/GameCard";
 
 const GamePage = () => {
   const { games } = useAppSelector((state) => state.games);
@@ -10,22 +11,7 @@ const GamePage = () => {
         {games &&
           games.map((game) => {
             return (
-              <Grid
-                key={game._id}
-                xs={3}
-                sx={{
-                  border: "1px solid black",
-                  borderRadius: "5px",
-                  margin: "1rem",
-                  padding: "1rem",
-                  width: "27%",
-                }}
-              >
-                <Link to={`/game/${game._id}`}>
-                  <p>{game.name ? game.name : "Nessun Nome"}</p>
-                </Link>
-                <p>{game.address}</p>
-              </Grid>
+              <GameCard key={game._id} game={game} />
             );
           })}
       </Grid>
